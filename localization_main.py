268ce2838585a37982d9620448cad8177f88004c
ivan-josef <ivanjosef141@gmail.com>
2026-09-15 16:39:19 -0300

    correção em aplicar_movimento

--- localization_main.py
+++ localization_main.py
@@ -22,10 +22,26 @@ def criar_particulas(n_particulas):
 
 # predição: aplicação do modelo de transição
 
-def aplicar_movimento(particula):
+def aplicar_movimento(particula,movimento):
 
-    for p in particula:
-        p['x'] += math.
+    passo = 0.4
+    erro_desloc = 0.05
+    rot = 20.0
+    erro_rot = 3
+
+    desloc = passo + random.uniform(-erro_desloc,erro_desloc)
+
+    if movimento == 'avancar':
+        particula['x'] += (desloc  * math.cos(particula['theta'])) 
+        particula['y'] += (desloc  * math.sin(particula['theta'])) 
+    elif movimento == 'girar':
+        particula['theta'] += math.radians(rot) + random.uniform(-math.radians(erro_rot),math.radians(erro_rot))
+
+def mover_particulas(particulas,movimento):
+
+    for p in particulas:
+        aplicar_movimento(p,movimento)
+
 
 
 
